427b17cacb7f7674fd016793e98cce480f9f10b2
MountainSpiritq <greenbull122@gmail.com>
2024-11-13 15:46:49 +0100

    ffs it works

--- index.js
+++ index.js
@@ -1,97 +1,91 @@
-import express from "express"
-import mysql from 'mysql2/promise'
-import cors from "cors"
-import { configDB } from "./configDB.js"
+import express from "express";
+import mysql from "mysql2/promise";
+import cors from "cors";
+import { configDB } from "./configDB.js";
 
-const PORT=8000
-let connection
+const PORT = 8000;
+let connection;
 
 try {
-    connection=await mysql.createConnection(configDB)
+  connection = await mysql.createConnection(configDB);
 } catch (error) {
-    console.log(error);
-    
+  console.log(error);
 }
 
-const app=express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/todos',async (req,resp)=>{
-    try {
-        const sql='SELECT * FROM todos ORDER BY TIMESTAMP DESC'
-        const [rows,fields]=await connection.execute(sql)
-        console.log(rows);
-        resp.send(rows)
-    } catch (error) {
-        console.log(error);
-        
-    }
-})
-app.post('/todos',async (req,resp)=>{
-    const{task}=req.body
-    if(!task) return resp.status(400).json({msg:'hianyos adatok!'})
+app.get("/todos", async (req, resp) => {
+  try {
+    const sql = "SELECT * FROM todos ORDER BY TIMESTAMP DESC";
+    const [rows, fields] = await connection.execute(sql);
+    console.log(rows);
+    resp.send(rows);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/todos", async (req, resp) => {
+  const { task } = req.body;
+  if (!task) return resp.status(400).json({ msg: "hianyos adatok!" });
 
-    try {
-        const sql='INSERT INTO todos (task) VALUES (?);'
-        const values=[task]
-        const [rows,fields]=await connection.execute(sql,values)
-        console.log(rows);
-        resp.status(200).json({msg:'sikeres adat beiras'})
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({msg:`server error: ${error}`})
-    }
-})
+  try {
+    const sql = "INSERT INTO todos (task) VALUES (?);";
+    const values = [task];
+    const [rows, fields] = await connection.execute(sql, values);
+    console.log(rows);
+    resp.status(200).json({ msg: "sikeres adat beiras" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({ msg: `server error: ${error}` });
+  }
+});
 
-app.delete('/todos/:id',async (req,resp)=>{
-    const{id}=req.params
-    
-    try {
-        const sql='DELETE FROM TODOS WHERE id=?'
-        const values=[id]
-        const [rows,fields]=await connection.execute(sql,values)
-        console.log(rows);
-        resp.status(200).json({msg:'sikeres adat torles'})
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({msg:`server error: ${error}`})
-    }
-})
+app.delete("/todos/:id", async (req, resp) => {
+  const { id } = req.params;
 
-app.put('/todos/:id',async (req,resp)=>{
-    const{id}=req.params
-    
-    try {
-        const sql='UPDATE todos SET completed=NOT completed WHERE id=? '
-        const values=[id]
-        const [rows,fields]=await connection.execute(sql,values)
-        console.log(rows);
-        resp.status(200).json({msg:'sikeres adat frissites'})
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({msg:`server error: ${error}`})
-    }
-})
+  try {
+    const sql = "DELETE FROM todos WHERE id=?";
+    const values = [id];
+    const [rows, fields] = await connection.execute(sql, values);
+    console.log(rows);
+    resp.status(200).json({ msg: "sikeres adat torles" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({ msg: `server error: ${error}` });
+  }
+});
 
-app.put('/todos/task/:id',async (req,resp)=>{
-    const{id}=req.params
-    const{task}=req.body
+app.put("/todos/completed/:id", async (req, resp) => {
+  const { id } = req.params;
 
-    
-    try {
-        const sql='UPDATE todos SET task=? WHERE id = ?'
-        const values=[task,id]
-        const [rows,fields]=await connection.execute(sql,values)
-        console.log(rows);
-        resp.status(200).json({msg:'sikeres adat frissites'})
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({msg:`server error: ${error}`})
-    }
-})
+  try {
+    const sql = "UPDATE todos SET completed=NOT completed WHERE id=? ";
+    const values = [id];
+    const [rows, fields] = await connection.execute(sql, values);
+    console.log(rows);
+    resp.status(200).json({ msg: "sikeres adat frissites" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({ msg: `server error: ${error}` });
+  }
+});
 
+app.put("/todos/task/:id", async (req, resp) => {
+  const { id } = req.params;
+  const { task } = req.body;
 
+  try {
+    const sql = "UPDATE todos SET task=? WHERE id = ?";
+    const values = [task, id];
+    const [rows, fields] = await connection.execute(sql, values);
+    console.log(rows);
+    resp.status(200).json({ msg: "sikeres adat frissites" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({ msg: `server error: ${error}` });
+  }
+});
 
-
-app.listen(PORT,()=>console.log(`server is Listnening on ${PORT}`))
+app.listen(PORT, () => console.log(`server is Listnening on ${PORT}`));
